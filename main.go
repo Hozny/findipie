cb83266b00e13ddebf0e2e250747fab0127efbbe
Yousof Hosny <yhosnysa@uwaterloo.ca>
2021-09-01 23:23:50 -0400

    Cleaned comments and removed tokens

--- main.go
+++ main.go
@@ -18,8 +18,6 @@ import (
 )
 
 var MAX_REQUEST_SIZE = 1048576
-var SPOTIFY_ID = "bf253aa4136a4f7f9caa0c9dabfb165c"
-var SPOTIFY_SECRET = "8cbaaea4aaeb451d8f4e3cbf9318e427"
 
 func getRouter(ctx context.Context, spotifyClient *spotify.Client) *mux.Router {
 	r := mux.NewRouter()
@@ -29,15 +27,10 @@ func getRouter(ctx context.Context, spotifyClient *spotify.Client) *mux.Router {
 }
 
 func main() {
-	var SPOTIFY_ID = os.Getenv("SPOTIFY_ID")
-	var SPOTIFY_SECRET = os.Getenv("SPOTIFY_SECRET")
-
 	ctx := context.Background()
 	config := &clientcredentials.Config{
-		// ClientID:     os.Getenv("SPOTIFY_ID"),
-		// ClientSecret: os.Getenv("SPOTIFY_SECRET"),
-		ClientID:     SPOTIFY_ID,
-		ClientSecret: SPOTIFY_SECRET,
+		ClientID:     os.Getenv("SPOTIFY_ID"),
+		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
 		TokenURL:     spotifyauth.TokenURL,
 	}
 	fmt.Println(os.Getenv("SPOTIFY_ID"), os.Getenv("SPOTIFY_SECRET"))
@@ -93,10 +86,6 @@ func searchPlaylists(ctx context.Context, spotifyClient *spotify.Client) http.Ha
 		username := string(searchRequest.Username)
 		fmt.Printf("Received search playlist request with playlist name: %s\n", username)
 
-		// fmt.Fprintf(w, "%+v", string(reqBody))
-		// fmt.Printf("%+v", string(reqBody))
-
-		// search for playlists and albums containing "holiday"
 		results, err := spotifyClient.Search(ctx, username, spotify.SearchTypePlaylist)
 		if err != nil {
 			log.Fatal(err)
@@ -104,12 +93,6 @@ func searchPlaylists(ctx context.Context, spotifyClient *spotify.Client) http.Ha
 
 		// handle playlist results
 		var searchResults []SearchPlaylistResult
-		if results.Playlists != nil {
-			fmt.Println("Playlists:")
-			for _, item := range results.Playlists.Playlists {
-				fmt.Println("   ", item.Name, item.Owner)
-			}
-		}
 		for _, item := range results.Playlists.Playlists {
 			var images []string
 			for _, image := range item.Images {
