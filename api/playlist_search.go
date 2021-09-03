2fcce2642fc49146fe8d31f0a022f8f8da9bf15d
Yousof Hosny <yhosnysa@uwaterloo.ca>
2021-09-02 20:15:50 -0400

    Separated api module

---
+++ api/playlist_search.go
@@ -0,0 +1,76 @@
+package SearchPlaylist
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/zmb3/spotify/v2"
+)
+
+type SearchPlaylistsRequest struct {
+	PlaylistName string `json:"playlistName"`
+}
+
+type SearchPlaylistsResult struct {
+	PlaylistName   string   `json:"username"`
+	PlaylistID     string   `json:"playlistID"`
+	OwnerName      string   `json:"ownerName"`
+	OwnerID        string   `json:"ownerID"`
+	PlaylistUrl    string   `json:"playlistURL"`
+	PlaylistImages []string `json:"playlistImages"`
+}
+
+type SearchPlaylistsResponse struct {
+	ResultsLength int                     `json:"resultsLength"`
+	Playlists     []SearchPlaylistsResult `json:"playlists"`
+}
+
+func SearchPlaylists(ctx context.Context, spotifyClient *spotify.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		reqBody, err := ioutil.ReadAll(io.LimitReader(r.Body, 12039123))
+		if err != nil {
+			fmt.Println("It broke")
+		}
+
+		var searchRequest SearchPlaylistsRequest
+		json.Unmarshal(reqBody, &searchRequest)
+		if err != nil {
+			fmt.Printf("Invalid request %+v", reqBody)
+			// TODO: handle error
+			return
+		}
+		playlistName := string(searchRequest.PlaylistName)
+		fmt.Printf("Received search playlist request with playlist name: %s\n", playlistName)
+
+		results, err := spotifyClient.Search(ctx, playlistName, spotify.SearchTypePlaylist)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// handle playlist results
+		var searchResults []SearchPlaylistsResult
+		for _, item := range results.Playlists.Playlists {
+			var images []string
+			for _, image := range item.Images {
+				images = append(images, image.URL)
+			}
+			searchResults = append(searchResults, SearchPlaylistsResult{
+				PlaylistName:   item.Name,
+				OwnerName:      item.Owner.DisplayName,
+				OwnerID:        item.Owner.ID,
+				PlaylistUrl:    item.ExternalURLs["spotify"],
+				PlaylistImages: images,
+			})
+		}
+		response := SearchPlaylistsResponse{
+			ResultsLength: len(searchResults),
+			Playlists:     searchResults,
+		}
+		json.NewEncoder(w).Encode(response)
+	}
+}
--- main.go
+++ main.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
-
 	"github.com/zmb3/spotify/v2"
 	spotifyauth "github.com/zmb3/spotify/v2/auth"
 	"golang.org/x/oauth2/clientcredentials"
+
+	"github.com/findipie/api"
 )
 
 var MAX_REQUEST_SIZE = 1048576
@@ -22,7 +20,7 @@ var MAX_REQUEST_SIZE = 1048576
 func getRouter(ctx context.Context, spotifyClient *spotify.Client) *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
-	r.HandleFunc("/search-playlists", searchPlaylists(ctx, spotifyClient))
+	r.HandleFunc("/search-playlists", api.SearchPlaylists(ctx, spotifyClient))
 	return r
 }
 
@@ -50,67 +48,4 @@ func main() {
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "HELLO")
-}
-
-type SearchPlaylistRequest struct {
-	PlaylistName string `json:"playlistName"`
-}
-
-type SearchPlaylistResult struct {
-	PlaylistName   string   `json:"username"`
-	PlaylistID     string   `json:"playlistID"`
-	OwnerName      string   `json:"ownerName"`
-	OwnerID        string   `json:"ownerID"`
-	PlaylistUrl    string   `json:"playlistURL"`
-	PlaylistImages []string `json:"playlistImages"`
-}
-
-type SearchPlaylistsResponse struct {
-	ResultsLength int                    `json:"resultsLength"`
-	Playlists     []SearchPlaylistResult `json:"playlists"`
-}
-
-func searchPlaylists(ctx context.Context, spotifyClient *spotify.Client) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		reqBody, err := ioutil.ReadAll(io.LimitReader(r.Body, 12039123))
-		if err != nil {
-			fmt.Println("It broke")
-		}
-
-		var searchRequest SearchPlaylistRequest
-		json.Unmarshal(reqBody, &searchRequest)
-		if err != nil {
-			fmt.Printf("Invalid request %+v", reqBody)
-			// TODO: return error code
-			return
-		}
-		playlistName := string(searchRequest.PlaylistName)
-		fmt.Printf("Received search playlist request with playlist name: %s\n", playlistName)
-
-		results, err := spotifyClient.Search(ctx, playlistName, spotify.SearchTypePlaylist)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// handle playlist results
-		var searchResults []SearchPlaylistResult
-		for _, item := range results.Playlists.Playlists {
-			var images []string
-			for _, image := range item.Images {
-				images = append(images, image.URL)
-			}
-			searchResults = append(searchResults, SearchPlaylistResult{
-				PlaylistName:   item.Name,
-				OwnerName:      item.Owner.DisplayName,
-				OwnerID:        item.Owner.ID,
-				PlaylistUrl:    item.ExternalURLs["spotify"],
-				PlaylistImages: images,
-			})
-		}
-		response := SearchPlaylistsResponse{
-			ResultsLength: len(searchResults),
-			Playlists:     searchResults,
-		}
-		json.NewEncoder(w).Encode(response)
-	}
 }
